efe45d4d61d2e62326cd394973abd7b866587438
mdrezwi <rezwimd377@gmail.com>
2026-02-14 18:39:23 +0530

    print all divison

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
 import pandas as pd
 
 df = pd.read_json("data.json")
-# print(df)
 data_structure = df.to_dict(orient="records")
-# 
-#   for s in data_structure:
-#       d = "Data Science"
-#       if s["division"] == d
-#           h = s["management"]["VP"]
-#           print(f"VP of {d} divison is {h}")
 
 for s in data_structure:
-     print(s)
-         
+     print(s["division"])
 
     
-    
-      
-    
-   
-    
-    
-    
-        
-        
-        
-        
-        
-#         
-# c = 0
-# 
-# for _ in data_structure:
-#     c = c+1
-# 
-# print(f"Total number of division is {c}")
-    
-    
-
-
-
-
-
-
-
-
-
-
