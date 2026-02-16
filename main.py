eb0d6ce13a0b54e0415c4da10b547af990c2166e
mdrezwi <rezwimd377@gmail.com>
2026-02-16 11:44:35 +0530

    count divisions done

--- main.py
+++ main.py
@@ -3,7 +3,51 @@ import pandas as pd
 df = pd.read_json("data.json")
 data_structure = df.to_dict(orient="records")
 
+# Goal : To count total number of divisions
+
+# setting an initial value to store count, initial value set to 0
+count=0
+
+# we will loop through all data
 for s in data_structure:
-     print(s["division"])
+    #print(s['division'])   # we don't need to print divisions to just count it 
+
+    count += 1      # we will increment external variable count by 1 in each loop
+                    # external variable count's value will keep increasing in each loop
+    
+print(f"Count : {count}")        # finally we will print the count
+                                 # we use f string to add some more information
+         
 
     
+    
+      
+    
+   
+    
+    
+    
+        
+        
+        
+        
+        
+#         
+# c = 0
+# 
+# for _ in data_structure:
+#     c = c+1
+# 
+# print(f"Total number of division is {c}")
+    
+    
+
+
+
+
+
+
+
+
+
+
